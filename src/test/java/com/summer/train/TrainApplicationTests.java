cb2449f74aacbd34955edb11c1e6c18a060ae908
qinpf <estsrfs0619@gmail.com>
2025-05-07 19:14:18 +0800

    初始化

---
+++ src/test/java/com/summer/train/TrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.summer.train;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
