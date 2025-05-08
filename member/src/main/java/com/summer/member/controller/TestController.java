c6e703b911043b3fc887f7ee9db55109468d4ea9
qinpf <estsrfs0619@gmail.com>
2025-05-08 14:50:44 +0800

    增加http测试文件，方便直接在IDEA中测试

--- member/src/main/java/com/summer/member/controller/TestController.java
+++ member/src/main/java/com/summer/member/controller/TestController.java
@@ -7,5 +7,5 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @GetMapping("/hello")
-    public String hello() {return "/hello";}
+    public String hello() {return "hello world";}
 }
