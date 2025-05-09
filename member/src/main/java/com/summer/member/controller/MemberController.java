1a20f0832d172415efcc3303f54bd9493e699a11
Administrator <estsrfs0619@gmail.com>
2025-05-09 19:41:00 +0800

    集成mybatis，访问数据库成功

---
+++ member/src/main/java/com/summer/member/controller/MemberController.java
@@ -0,0 +1,18 @@
+package com.summer.member.controller;
+
+import com.summer.member.service.MemberService;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/member")
+public class MemberController {
+
+    @Resource
+    private MemberService memberService;
+
+    @GetMapping("/count")
+    public int count() {return memberService.count();}
+}
